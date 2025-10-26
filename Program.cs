1bbdcdb2b81a6c98e9e0418d27e3cf3d4413464a
abdallaah97 <39929445+abdallaah97@users.noreply.github.com>
2025-10-26 18:44:52 +0300

    test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Test Changes");
+Console.WriteLine("change from another memeber");
